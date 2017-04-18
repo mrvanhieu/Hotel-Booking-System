3911517b4bdb572a428ea31ba7b61f966f5e7c81
Hieu Ho <mrvanhieu@gmail.com>
2017-04-18 15:39:05 -0500

    Apply Sington design pattern #rSNbIIKX

--- src/com/edu/mum/hbs/dao/DaoAbstract.java
+++ src/com/edu/mum/hbs/dao/DaoAbstract.java
@@ -3,5 +3,7 @@ package com.edu.mum.hbs.dao;
 import com.edu.mum.hbs.util.SqliteUtil;
 
 public abstract class DaoAbstract {
-	protected SqliteUtil db = new SqliteUtil();
+	// Singleton design pattern
+	//protected SqliteUtil db = new SqliteUtil();
+	protected SqliteUtil db = SqliteUtil.getInstance();
 }
