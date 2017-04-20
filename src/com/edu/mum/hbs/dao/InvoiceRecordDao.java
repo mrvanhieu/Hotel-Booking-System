36acef7faeb5fa7dfc7f3947a2bf2111ae45f5b8
Thanh Le <letienthanh@gmail.com>
2017-04-20 14:23:18 -0500

    refactor Profit to Revenue

--- src/com/edu/mum/hbs/dao/InvoiceRecordDao.java
+++ src/com/edu/mum/hbs/dao/InvoiceRecordDao.java
@@ -1,7 +1,7 @@
 package com.edu.mum.hbs.dao;
 
 import com.edu.mum.hbs.entity.InvoiceRecord;
-import com.edu.mum.hbs.entity.Profit;
+import com.edu.mum.hbs.entity.Revenue;
 import com.edu.mum.hbs.util.SqliteUtil;
 import com.edu.mum.hbs.util.SqliteUtil.FilterCondition;
 
@@ -13,102 +13,101 @@ import java.util.List;
 import java.util.Map;
 
 public class InvoiceRecordDao extends DaoAbstract {
-    private static final String TABLE_NAME = InvoiceRecord.TABLE_NAME;
+	private static final String TABLE_NAME = InvoiceRecord.TABLE_NAME;
 
-    InvoiceRecordDao() {
-    }
+	InvoiceRecordDao() {
+	}
 
-    public void addInvoice(InvoiceRecord invoiceRecord) {
-        LinkedHashMap<String, Object> map = new LinkedHashMap<String, Object>();
-        map.put(InvoiceRecord.PASSPORT_OR_ID, invoiceRecord.getPassportOrId());
-        map.put(InvoiceRecord.ROOM_NUMBER, invoiceRecord.getRoomNumber());
-        map.put(InvoiceRecord.CHECK_IN_DATE, invoiceRecord.getCheckInDate());
-        map.put(InvoiceRecord.CHECK_OUT_DATE, invoiceRecord.getCheckOutDate());
-        map.put(InvoiceRecord.ROOM_AMOUNT, invoiceRecord.getRoomAmount());
-        map.put(InvoiceRecord.SERVICE_AMOUNT, invoiceRecord.getServiceAmount());
-        map.put(InvoiceRecord.TOTAL_AMOUNT, invoiceRecord.getTotalAmount());
+	public void addInvoice(InvoiceRecord invoiceRecord) {
+		LinkedHashMap<String, Object> map = new LinkedHashMap<String, Object>();
+		map.put(InvoiceRecord.PASSPORT_OR_ID, invoiceRecord.getPassportOrId());
+		map.put(InvoiceRecord.ROOM_NUMBER, invoiceRecord.getRoomNumber());
+		map.put(InvoiceRecord.CHECK_IN_DATE, invoiceRecord.getCheckInDate());
+		map.put(InvoiceRecord.CHECK_OUT_DATE, invoiceRecord.getCheckOutDate());
+		map.put(InvoiceRecord.ROOM_AMOUNT, invoiceRecord.getRoomAmount());
+		map.put(InvoiceRecord.SERVICE_AMOUNT, invoiceRecord.getServiceAmount());
+		map.put(InvoiceRecord.TOTAL_AMOUNT, invoiceRecord.getTotalAmount());
 
-        db.insertRow(TABLE_NAME, map, false);
-    }
+		db.insertRow(TABLE_NAME, map, false);
+	}
 
-    public List<InvoiceRecord> getAllInvoiceRecords() {
-        List<InvoiceRecord> invoiceRecords = new ArrayList<InvoiceRecord>();
-        List<Map<String, Object>> objects = db.get(TABLE_NAME, null, null);
+	public List<InvoiceRecord> getAllInvoiceRecords() {
+		List<InvoiceRecord> invoiceRecords = new ArrayList<InvoiceRecord>();
+		List<Map<String, Object>> objects = db.get(TABLE_NAME, null, null);
 
-        if (objects.size() > 0) {
-            for (Map<String, Object> ob : objects) {
-                InvoiceRecord invoiceRecord = new InvoiceRecord();
-                invoiceRecord.setPassportOrId((String) ob.get(InvoiceRecord.PASSPORT_OR_ID));
-                invoiceRecord.setRoomNumber((String) ob.get(InvoiceRecord.ROOM_NUMBER));
-                invoiceRecord.setCheckInDate((String) ob.get(InvoiceRecord.CHECK_IN_DATE));
-                invoiceRecord.setCheckOutDate((String) ob.get(InvoiceRecord.CHECK_OUT_DATE));
-                invoiceRecord.setRoomAmount((Double) ob.get(InvoiceRecord.ROOM_AMOUNT));
-                invoiceRecord.setServiceAmount((Double) ob.get(InvoiceRecord.SERVICE_AMOUNT));
-                invoiceRecord.setTotalAmount((Double) ob.get(InvoiceRecord.TOTAL_AMOUNT));
+		if (objects.size() > 0) {
+			for (Map<String, Object> ob : objects) {
+				InvoiceRecord invoiceRecord = new InvoiceRecord();
+				invoiceRecord.setPassportOrId((String) ob.get(InvoiceRecord.PASSPORT_OR_ID));
+				invoiceRecord.setRoomNumber((String) ob.get(InvoiceRecord.ROOM_NUMBER));
+				invoiceRecord.setCheckInDate((String) ob.get(InvoiceRecord.CHECK_IN_DATE));
+				invoiceRecord.setCheckOutDate((String) ob.get(InvoiceRecord.CHECK_OUT_DATE));
+				invoiceRecord.setRoomAmount((Double) ob.get(InvoiceRecord.ROOM_AMOUNT));
+				invoiceRecord.setServiceAmount((Double) ob.get(InvoiceRecord.SERVICE_AMOUNT));
+				invoiceRecord.setTotalAmount((Double) ob.get(InvoiceRecord.TOTAL_AMOUNT));
 
-                invoiceRecords.add(invoiceRecord);
-            }
-        }
-        return invoiceRecords;
-    }
+				invoiceRecords.add(invoiceRecord);
+			}
+		}
+		return invoiceRecords;
+	}
 
-    public List<InvoiceRecord> getAllInvoiceRecordsFromToDate(String fromDate, String toDate) {
-        List<InvoiceRecord> invoiceRecords = new ArrayList<InvoiceRecord>();
-        FilterCondition condition = new SqliteUtil.FilterCondition(SqliteUtil.LogicalOperator.AND);
-        condition.addCondition(InvoiceRecord.CHECK_IN_DATE, SqliteUtil.GREATER_EQUALS, fromDate);
-        condition.addCondition(InvoiceRecord.CHECK_OUT_DATE, SqliteUtil.LESS_EQUALS, toDate);
-        List<Map<String, Object>> objects = db.get(TABLE_NAME, null, condition);
+	public List<InvoiceRecord> getAllInvoiceRecordsFromToDate(String fromDate, String toDate) {
+		List<InvoiceRecord> invoiceRecords = new ArrayList<InvoiceRecord>();
+		FilterCondition condition = new SqliteUtil.FilterCondition(SqliteUtil.LogicalOperator.AND);
+		condition.addCondition(InvoiceRecord.CHECK_IN_DATE, SqliteUtil.GREATER_EQUALS, fromDate);
+		condition.addCondition(InvoiceRecord.CHECK_OUT_DATE, SqliteUtil.LESS_EQUALS, toDate);
+		List<Map<String, Object>> objects = db.get(TABLE_NAME, null, condition);
 
-        if (objects.size() > 0) {
-            for (Map<String, Object> ob : objects) {
-                InvoiceRecord invoiceRecord = new InvoiceRecord();
-                invoiceRecord.setPassportOrId((String) ob.get(InvoiceRecord.PASSPORT_OR_ID));
-                invoiceRecord.setRoomNumber((String) ob.get(InvoiceRecord.ROOM_NUMBER));
-                invoiceRecord.setCheckInDate((String) ob.get(InvoiceRecord.CHECK_IN_DATE));
-                invoiceRecord.setCheckOutDate((String) ob.get(InvoiceRecord.CHECK_OUT_DATE));
-                invoiceRecord.setRoomAmount((Double) ob.get(InvoiceRecord.ROOM_AMOUNT));
-                invoiceRecord.setServiceAmount((Double) ob.get(InvoiceRecord.SERVICE_AMOUNT));
-                invoiceRecord.setTotalAmount((Double) ob.get(InvoiceRecord.TOTAL_AMOUNT));
+		if (objects.size() > 0) {
+			for (Map<String, Object> ob : objects) {
+				InvoiceRecord invoiceRecord = new InvoiceRecord();
+				invoiceRecord.setPassportOrId((String) ob.get(InvoiceRecord.PASSPORT_OR_ID));
+				invoiceRecord.setRoomNumber((String) ob.get(InvoiceRecord.ROOM_NUMBER));
+				invoiceRecord.setCheckInDate((String) ob.get(InvoiceRecord.CHECK_IN_DATE));
+				invoiceRecord.setCheckOutDate((String) ob.get(InvoiceRecord.CHECK_OUT_DATE));
+				invoiceRecord.setRoomAmount((Double) ob.get(InvoiceRecord.ROOM_AMOUNT));
+				invoiceRecord.setServiceAmount((Double) ob.get(InvoiceRecord.SERVICE_AMOUNT));
+				invoiceRecord.setTotalAmount((Double) ob.get(InvoiceRecord.TOTAL_AMOUNT));
 
-                invoiceRecords.add(invoiceRecord);
-            }
-        }
-        return invoiceRecords;
-    }
+				invoiceRecords.add(invoiceRecord);
+			}
+		}
+		return invoiceRecords;
+	}
 
-    public List<Profit> getAllProfitRecords() {
-        List<InvoiceRecord> invoiceRecords = getAllInvoiceRecords();
-        List<Profit> profitRecords = new ArrayList<Profit>();
+	public List<Revenue> getAllRevenueRecords() {
+		List<InvoiceRecord> invoiceRecords = getAllInvoiceRecords();
+		List<Revenue> revenueRecords = new ArrayList<Revenue>();
 
-        if (invoiceRecords.size() > 0) {
-            for (InvoiceRecord invoice : invoiceRecords) {
-                profitRecords.add(new Profit(invoice));
-            }
-        }
+		if (invoiceRecords.size() > 0) {
+			for (InvoiceRecord invoice : invoiceRecords) {
+				revenueRecords.add(new Revenue(invoice));
+			}
+		}
 
-        return profitRecords;
-    }
+		return revenueRecords;
+	}
 
-    public List<Profit> getAllProfitRecordsFromToDate(String fromDate, String toDate) {
-        List<InvoiceRecord> invoiceRecords = getAllInvoiceRecordsFromToDate(fromDate, toDate);
-        List<Profit> profitRecords = new ArrayList<Profit>();
+	public List<Revenue> getAllRevenueRecordsFromToDate(String fromDate, String toDate) {
+		List<InvoiceRecord> invoiceRecords = getAllInvoiceRecordsFromToDate(fromDate, toDate);
+		List<Revenue> revenueRecords = new ArrayList<Revenue>();
 
-        if (invoiceRecords.size() > 0) {
-            for (InvoiceRecord invoice : invoiceRecords) {
-                profitRecords.add(new Profit(invoice));
-            }
-        }
+		if (invoiceRecords.size() > 0) {
+			for (InvoiceRecord invoice : invoiceRecords) {
+				revenueRecords.add(new Revenue(invoice));
+			}
+		}
 
-        return profitRecords;
-    }
+		return revenueRecords;
+	}
 
-    public static void main(String[] argv) {
-        LocalDate startDate = LocalDate.now().minusDays(4);
-        LocalDate endDate = LocalDate.now();
+	public static void main(String[] argv) {
+		LocalDate startDate = LocalDate.now().minusDays(4);
+		LocalDate endDate = LocalDate.now();
 
-        long days = Period.between(startDate, endDate).getDays();
-        System.out.println(days);
-    }
-
+		long days = Period.between(startDate, endDate).getDays();
+		System.out.println(days);
+	}
 
 }
